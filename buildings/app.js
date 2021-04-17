f832bc800601d137c26d470477b68167a6680a8e
Niki <nicolette88@gmail.com>
2021-04-17 18:06:31 +0200

    Javascript elkezdve

---
+++ buildings/app.js
@@ -0,0 +1,14 @@
+console.log('hello')
+
+document.querySelector('.menu-button').addEventListener('click', function () {
+
+  console.log('clicked!');
+  document.querySelector('body').classList.add('js-menu-open');
+  document.querySelector('.close-menu').classList.remove('js-hidden');
+});
+
+document.querySelector('.close-menu').addEventListener('click', function () {
+
+  document.querySelector('body').classList.remove('js-menu-open');
+  document.querySelector('.close-menu').classList.add('js-hidden');
+});
